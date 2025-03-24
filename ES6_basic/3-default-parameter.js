94776522e544f55bae471650600dff0843016edb
Braga <ibrahim.houmaidi@gmail.com>
2025-03-24 14:07:14 +0100

    task 3

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,12 @@
+/**
+ * Tâche 3: Valeurs des paramètres par défaut
+ * Démontre la syntaxe des paramètres par défaut ES6 dans la déclaration de fonction
+ * @param {number} initialNumber - Valeur initiale sans défaut
+ * @param {number} expansion1989 - Paramètre avec valeur par défaut 89
+ * @param {number} expansion2019 - Paramètre avec valeur par défaut 19
+ * @return {number} Somme de tous les paramètres
+ */
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  // Retourner la somme de tous les paramètres (en utilisant les valeurs par défaut si non fournies)
+  return initialNumber + expansion1989 + expansion2019;
+}
