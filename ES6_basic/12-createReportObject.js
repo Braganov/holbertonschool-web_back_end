9cf98811dfd4b87ca78ef12a9438894ae7b0367c
Braga <ibrahim.houmaidi@gmail.com>
2025-03-25 13:51:13 +0100

    update

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,3 @@
-/**
- * Tâche 12: Créons un objet rapport
- * Fonction qui crée un objet rapport à partir d'une liste d'employés
- * @param {Object} employeesList - Objet contenant les départements et leurs employés
- * @return {Object} Objet rapport avec la liste des employés et une méthode pour compter les départements
- */
 export default function createReportObject(employeesList) {
   // Création d'un objet avec une propriété et une méthode
   return {
--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,13 +1,4 @@
 import getBudgetObject from './7-getBudgetObject.js';
-
-/**
- * Tâche 9: Propriétés de méthode ES6
- * Fonction qui crée un objet budget complet avec des méthodes
- * @param {number} income - Valeur du revenu
- * @param {number} gdp - Valeur du PIB
- * @param {number} capita - Valeur par habitant
- * @return {Object} Objet budget complet avec des méthodes utilisant la syntaxe ES6
- */
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
 
