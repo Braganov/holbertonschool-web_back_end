be7c26bf92d22407d8e76e3fae142c5aef44e8b0
Braga <ibrahim.houmaidi@gmail.com>
2025-03-25 13:53:43 +0100

    update

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,4 @@
 export default function getBudgetObject(income, gdp, capita) {
-  // Utilisation de la syntaxe abrégée des propriétés d'objet (le nom de la variable devient le nom de la propriété)
   const budget = {
     income,
     gdp,
--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,9 +1,6 @@
 import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-
-  // Utilisation de la syntaxe des méthodes ES6 (sans le mot-clé 'function')
-  // et de l'opérateur spread pour combiner les objets
   const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
