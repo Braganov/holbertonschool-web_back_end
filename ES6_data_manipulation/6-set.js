ec49eb2f9c61149ac72ff7ce70db6389f6b05560
Braga <ibrahim.houmaidi@gmail.com>
2025-04-02 13:12:11 +0200

    task 6

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a Set from an array
+ * @param {Array} array - The array to convert to a Set
+ * @return {Set} A Set created from the array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
