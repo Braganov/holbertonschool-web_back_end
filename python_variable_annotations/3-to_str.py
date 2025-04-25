ed361ff4af8c368cff8671b8b53af3db85cf38b5
Braga <ibrahim.houmaidi@gmail.com>
2025-04-25 16:43:08 +0200

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for task 3 - Basic Annotations - To String."""
+
+
+def to_str(n: float) -> str:
+    """Convert a floating point number to its string representation.
+
+    Args:
+        n: A floating point number
+
+    Returns:
+        The string representation of the input
+    """
+    return str(n)
