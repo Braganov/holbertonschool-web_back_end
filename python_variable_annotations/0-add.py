f57372182a0ae4eff76fab4fe5d295f92498fe0a
Braga <ibrahim.houmaidi@gmail.com>
2025-04-25 16:41:50 +0200

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for task 0 - Basic Annotations - Add."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floating point numbers and return their sum.
+
+    Args:
+        a: The first float
+        b: The second float
+
+    Returns:
+        The sum of a and b as a float
+    """
+    return a + b
