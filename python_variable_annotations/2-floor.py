f103c62262f02b1161cda22d971512498dc66d6a
Braga <ibrahim.houmaidi@gmail.com>
2025-04-25 16:42:57 +0200

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for task 2 - Basic Annotations - Floor."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a floating point number.
+
+    Args:
+        n: A floating point number
+
+    Returns:
+        The floor of the input as an integer
+    """
+    return math.floor(n)
