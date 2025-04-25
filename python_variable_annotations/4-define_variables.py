866e51c946abf7688e91e4f887953341ff846478
Braga <ibrahim.houmaidi@gmail.com>
2025-04-25 16:43:32 +0200

    task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for task 4 - Define variables with specific values and types."""
+
+# Define and annotate the variables
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
