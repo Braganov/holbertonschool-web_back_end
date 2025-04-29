98c3edec59d26009609632f4e7ac1f5cbb225466
Braga <ibrahim.houmaidi@gmail.com>
2025-04-29 11:19:35 +0200

    update code

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -12,5 +12,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     then yields a random number.
     """
     for _ in range(10):
+        i = random.uniform(0, 10)
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield i
